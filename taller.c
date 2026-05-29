8a59dd13c100ae4a07f43bb2224f7c0429a82e41
renatin <cviche.ren@gmail.com>
2026-05-29 14:50:20 -0400

    olaola

--- taller.c
+++ taller.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct datos{
+    char* numeropaciente;
+    int edad, prioridad, tickllegada, ticksestimados;
+    float p_empeorar, p_mejorar;
+} Datos;
+
+typedef struct nododoble{
+    Datos paciente;
+    struct nododoble* siguiente;
+    struct nododoble* anterior;
+} NodoDoble;
+
+typedef struct nodo{
+    Datos paciente;
+    struct nodo* siguiente;
+} Nodo;
 
 int main(){
 
