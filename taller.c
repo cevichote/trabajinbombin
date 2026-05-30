4ce5272506c67c014029b1853fee2661a6ab0e50
renatin <cviche.ren@gmail.com>
2026-05-30 16:18:34 -0400

    olamundo

--- taller.c
+++ taller.c
@@ -20,12 +20,21 @@ typedef struct nodo{
     struct nodo* siguiente;
 } Nodo;
 
-void leerarchivo(Nodo**, int); /*Se lee el archivo y se agregan pacientes a la cola cada vez que el tiempo aumenta en 1 tick, para simular bien los tiempos de llegada*/
+void leerarchivo(Nodo**, int, FILE*); /*Se lee el archivo y se agregan pacientes a la cola cada vez que el tiempo aumenta en 1 tick, para simular bien los tiempos de llegada*/
 void apilar(Nodo**);
 void encolar(Nodo**);
 Nodo* desencolar(Nodo**); 
 
 int main(){
-
+    int medicos, pacientes;
+    FILE *entrada;
+    entrada = fopen ("Entrada.txt", "r");
+    fscanf (entrada, "%d %d", &medicos, &pacientes);
+    printf ("%d %d", medicos, pacientes);
+    fclose (entrada);
     return 0;
+}
+
+void leerarchivo (Nodo** cola, int tiempo, FILE* archivo){
+    
 }
