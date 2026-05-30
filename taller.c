a910465a882d213ee61012c0fb5a08d182ac930a
renatin <cviche.ren@gmail.com>
2026-05-29 20:48:19 -0400

    olaaaa

--- taller.c
+++ taller.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct datos{
-    char* numeropaciente;
+    char* identificador;
     int edad, prioridad, tickllegada, ticksestimados;
     float p_empeorar, p_mejorar;
 } Datos;
@@ -18,6 +19,11 @@ typedef struct nodo{
     Datos paciente;
     struct nodo* siguiente;
 } Nodo;
+
+void leerarchivo(Nodo**, int); /*Se lee el archivo y se agregan pacientes a la cola cada vez que el tiempo aumenta en 1 tick, para simular bien los tiempos de llegada*/
+void apilar(Nodo**);
+void encolar(Nodo**);
+Nodo* desencolar(Nodo**); 
 
 int main(){
 
